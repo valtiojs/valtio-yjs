9bb9ca595d905c15842bf2e495be6c913b9725e4
daishi <daishi@axlight.com>
2021-07-19 11:39:33 +0900

    refactor eliminating duplicated code

--- src/index.ts
+++ src/index.ts
@@ -7,65 +7,67 @@ const isObject = (x: unknown): x is Record<string, unknown> => (
 );
 
 export const bindProxyAndYMap = <T>(p: Record<string, T>, y: Y.Map<T>) => {
-  // initialize from y
-  y.forEach((yv /* ydoc value */, k) => {
+  const setYValueToP = (yv: T, k: string) => {
     if (yv instanceof Y.Array) {
-      const pv = proxy(yv.toJSON());
-      bindProxyAndYArray(pv, yv);
-      p[k] = pv as unknown as T;
+      const json = yv.toJSON();
+      if (!deepEqual(p[k], json)) {
+        const pv = proxy(json);
+        bindProxyAndYArray(pv, yv);
+        p[k] = pv as unknown as T;
+      }
     } else if (yv instanceof Y.Map) {
-      const pv = proxy(yv.toJSON());
-      bindProxyAndYMap(pv, yv);
-      p[k] = pv as unknown as T;
+      const json = yv.toJSON();
+      if (!deepEqual(p[k], json)) {
+        const pv = proxy(json);
+        bindProxyAndYMap(pv, yv);
+        p[k] = pv as unknown as T;
+      }
     } else if (typeof yv === 'string' || typeof yv === 'number' || typeof yv === 'boolean') {
       p[k] = yv;
     } else {
       throw new Error('unsupported y type');
     }
-  });
+  };
 
-  // initialize from p
-  Object.keys(p).forEach((k) => {
-    const pv /* proxy value */ = p[k];
+  const setPValueToY = (pv: T, k: string) => {
     if (Array.isArray(pv)) {
-      const yv = new Y.Array();
-      bindProxyAndYArray(pv, yv);
-      y.set(k, yv as unknown as T);
+      const prev = y.get(k);
+      if (!(prev instanceof Y.Array) || !deepEqual(prev.toJSON(), pv)) {
+        const yv = new Y.Array();
+        bindProxyAndYArray(pv, yv);
+        y.set(k, yv as unknown as T);
+      }
     } else if (isObject(pv)) {
-      const yv = new Y.Map();
-      bindProxyAndYMap(pv, yv);
-      y.set(k, yv as unknown as T);
+      const prev = y.get(k);
+      if (!(prev instanceof Y.Map) || !deepEqual(prev.toJSON(), pv)) {
+        const yv = new Y.Map();
+        bindProxyAndYMap(pv, yv);
+        y.set(k, yv as unknown as T);
+      }
     } else if (typeof pv === 'string' || typeof pv === 'number' || typeof pv === 'boolean') {
       y.set(k, pv);
     } else {
       throw new Error('unsupported p type');
     }
+  };
+
+  // initialize from y
+  y.forEach(setYValueToP);
+
+  // initialize from p
+  Object.keys(p).forEach((k) => {
+    const pv = p[k];
+    setPValueToY(pv, k);
   });
 
   // subscribe y
   y.observe((event) => {
     event.keysChanged.forEach((k) => {
-      const yv /* ydoc value */ = y.get(k);
+      const yv = y.get(k);
       if (yv === undefined) {
         delete p[k];
-      } else if (yv instanceof Y.Array) {
-        const json = yv.toJSON();
-        if (!deepEqual(p[k], json)) {
-          const pv = proxy(json);
-          bindProxyAndYArray(pv, yv);
-          p[k] = pv as unknown as T;
-        }
-      } else if (yv instanceof Y.Map) {
-        const json = yv.toJSON();
-        if (!deepEqual(p[k], json)) {
-          const pv = proxy(json);
-          bindProxyAndYMap(pv, yv);
-          p[k] = pv as unknown as T;
-        }
-      } else if (typeof yv === 'string' || typeof yv === 'number' || typeof yv === 'boolean') {
-        p[k] = yv;
       } else {
-        throw new Error('unsupported y type');
+        setYValueToP(yv, k);
       }
     });
   });
@@ -81,26 +83,8 @@ export const bindProxyAndYMap = <T>(p: Record<string, T>, y: Y.Map<T>) => {
       if (op[0] === 'delete') {
         y.delete(k);
       } else if (op[0] === 'set') {
-        const pv /* proxy value */ = p[k];
-        if (Array.isArray(pv)) {
-          const prev = y.get(k);
-          if (!(prev instanceof Y.Array) || !deepEqual(prev.toJSON(), pv)) {
-            const yv = new Y.Array();
-            bindProxyAndYArray(pv, yv);
-            y.set(k, yv as unknown as T);
-          }
-        } else if (isObject(pv)) {
-          const prev = y.get(k);
-          if (!(prev instanceof Y.Map) || !deepEqual(prev.toJSON(), pv)) {
-            const yv = new Y.Map();
-            bindProxyAndYMap(pv, yv);
-            y.set(k, yv as unknown as T);
-          }
-        } else if (typeof pv === 'string' || typeof pv === 'number' || typeof pv === 'boolean') {
-          y.set(k, pv);
-        } else {
-          throw new Error('unsupported p type');
-        }
+        const pv = p[k];
+        setPValueToY(pv, k);
       }
     });
   });
