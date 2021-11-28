589033a8b66e78e958199cb0350f4f0cf1e89ace
Namit Chadha <namit.chadha@gmail.com>
2021-11-28 00:06:22 +0000

    Support nested map set (#15)
    * workaround (lower perf than object ref checking) to unblock progress
    
    * tidy
    
    * latest yarn.lock
    
    * use WeakMap
    
    * reorder

---
+++ __tests__/07_nested_map_spec.ts
@@ -0,0 +1,44 @@
+import * as Y from 'yjs';
+import { proxy } from 'valtio/vanilla';
+import { bindProxyAndYMap } from '../src/index';
+
+describe('issue #14', () => {
+  it('nested map direct set', async () => {
+    const doc = new Y.Doc();
+    const p = proxy({ items: { item1: { color: 'blue' } } });
+    const m = doc.getMap('map');
+
+    bindProxyAndYMap(p, m);
+
+    p.items.item1.color = 'red';
+    await Promise.resolve();
+
+    expect(m.get('items').get('item1').get('color')).toStrictEqual('red');
+  });
+
+  it('nested map 1 level outer set', async () => {
+    const doc = new Y.Doc();
+    const p = proxy({ items: { item1: { color: 'blue' } } });
+    const m = doc.getMap('map');
+
+    bindProxyAndYMap(p, m);
+
+    p.items.item1 = { color: 'red' };
+    await Promise.resolve();
+
+    expect(m.get('items').get('item1').get('color')).toStrictEqual('red');
+  });
+
+  it('nested map 2 level outer set', async () => {
+    const doc = new Y.Doc();
+    const p = proxy({ items: { item1: { color: 'blue' } } });
+    const m = doc.getMap('map');
+
+    bindProxyAndYMap(p, m);
+
+    p.items = { item1: { color: 'red' } };
+    await Promise.resolve();
+
+    expect(m.get('items').get('item1').get('color')).toStrictEqual('red');
+  });
+});
